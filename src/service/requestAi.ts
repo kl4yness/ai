54daa844e851266e2d19ce15c6345651ff8d7e47
kl4yness <120795795+kl4yness@users.noreply.github.com>
2026-03-20 13:12:26 +0500

    Update requestAi.ts

--- src/service/requestAi.ts
+++ src/service/requestAi.ts
@@ -84,7 +84,10 @@ export async function requestAI(messages: Message[], chatId: string) {
     ];
 
     // 🔥 НОРМАЛЬНЫЕ модели
-    const models = ["openai/gpt-oss-120b:free"];
+    const models = ["deepseek/deepseek-chat-v3-0324:free",     
+  "google/gemini-2.0-flash-exp:free",         
+  "openai/gpt-oss-120b:free",          
+  "qwen/qwen3-next-80b-a3b-instruct:free"];
 
     let lastError: any = null;
 
